14329e8a03abf42de26b5516e24157237a93c5f3
Nazarii Lishchynskiy <132710119+Nazarioo123@users.noreply.github.com>
2024-11-26 19:37:12 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Fitness_Diary.ContentPages; 
+
+namespace Fitness_Diary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new ProfilPage();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Fitness_Diary.ContentPages;
+
+
+namespace Fitness_Diary
+{
+    
+
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("ProfilPage", typeof(ProfilPage));
+        }
+
+    }
+}
